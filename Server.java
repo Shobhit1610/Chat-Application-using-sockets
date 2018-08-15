4d5cc14c1573db44692bc6678de64e25dbc9c561
Shobhit Jain <shobhitjain1610@gmail.com>
2018-08-15 22:11:56 +0530

    Server.java file is created
    Contains code of simple server that accepts the client messages

---
+++ Server.java
@@ -0,0 +1,25 @@
+package com.chatapp.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+        public static void main(String[] args)
+        {
+            try(ServerSocket serverSocket = new ServerSocket(5000))
+            {
+                while(true)
+                {
+                    new ClientThread(serverSocket.accept()).start();
+                }
+            } catch(IOException e) {
+                System.out.println("Server exception " + e.getMessage());
+            }
+        }
+}
+
